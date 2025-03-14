d13101b3c798d55d500b4dbf1becc0772fc2a9ad
XynixDev <piotrmroz244@gmail.com>
2025-03-14 12:01:56 +0100

    Add files via upload
    Signed-off-by: XynixDev <piotrmroz244@gmail.com>

---
+++ giles.cpp
@@ -0,0 +1,6 @@
+"engine" e> start in open "main" e/1 reload script 1 minute 1s .working no .working yes
+110 saydey 3# ^ `open auto main or engine 101 error "engine" e> start in open "main" e/1 reload script 1 minute 1s .working no .working yes"engine" e> start in open "main" e/1 reload script 1 minute 1s .working no .working yes
+110 saydey 3# ^ `open auto main or engine 101 error
+110 saydey 3# ^ `open auto main or engine 101 error"engine" e> start in open "main" e/1 reload script 1 minute 1s .working no .working yes"engine" e> start in open "main" e/1 reload script 1 minute 1s .working no .working yes
+110 saydey 3# ^ `open auto main or engine 101 error
+110 saydey 3# ^ `open auto main or engine 101 error
